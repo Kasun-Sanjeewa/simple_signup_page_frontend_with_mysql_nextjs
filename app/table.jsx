eeb3356c711c68088c223a4e492483e7f73ed617
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-16 02:26:02 +0530

    Frontend intigrated with Backend

--- app/table.jsx
+++ app/table.jsx
@@ -7,54 +7,49 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-
-
 export default function BasicTable({ users }) {
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                {/* Table Head */}
                 <TableHead sx={{ backgroundColor: '#1976d2' }}>
                     <TableRow>
-                        <TableCell align="right" sx={{ color: '#fff', borderLeft: '1px silid' }}>Id</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Name</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Gender</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Email</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Contact</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Address</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>State</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Country</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Uniqueregid</TableCell>
-                        <TableCell align="right" sx={{ color: '#fff' }}>Imagename</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Id</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Name</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Gender</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Email</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Contact</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Address</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>State</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Country</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Uniqueregid</TableCell>
+                        <TableCell align="center" sx={{ color: '#fff', fontWeight: 'bold', border: '1px solid #fff' }}>Imagename</TableCell>
                     </TableRow>
                 </TableHead>
+
+                {/* Table Body */}
                 <TableBody>
                     {
                         users.length > 0 ? users.map((user) => {
                             return (
-                                <TableRow
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell align="right">{user.id}</TableCell>
-                                    <TableCell align="right">{user.name}</TableCell>
-                                    <TableCell align="right">{user.gender}</TableCell>
-                                    <TableCell align="right">{user.email}</TableCell>
-                                    <TableCell align="right">{user.contact}</TableCell>
-                                    <TableCell align="right">{user.address}</TableCell>
-                                    <TableCell align="right">{user.state}</TableCell>
-                                    <TableCell align="right">{user.country}</TableCell>
-                                    <TableCell align="right">{user.uniqueregid}</TableCell>
-                                    <TableCell align="right">{user.imagename}</TableCell>
-
+                                <TableRow key={user.id}>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.id}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.name}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.gender}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.email}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.contact}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.address}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.state}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.country}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.uniqueregid}</TableCell>
+                                    <TableCell align="center" sx={{ border: '1px solid #ccc' }}>{user.imagename}</TableCell>
                                 </TableRow>
                             )
                         }) : (
                             <TableRow>
-
-                                <TableCell component='th' scope="row">No Data</TableCell>
-
+                                <TableCell colSpan={10} align="center" sx={{ border: '1px solid #ccc' }}>No Data</TableCell>
                             </TableRow>
                         )
-
                     }
                 </TableBody>
             </Table>
